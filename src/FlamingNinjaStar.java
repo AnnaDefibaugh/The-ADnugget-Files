f01da46a2083aa2c8edbfff68e158d38fcbbbee3
Anna <officialadnugget@gmail.com>
2015-06-18 18:28:06 -0700

    "Cool Stuff"

--- src/FlamingNinjaStar.java
+++ src/FlamingNinjaStar.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+
+import org.teachingextensions.logo.Colors;
 import org.teachingextensions.logo.Tortoise;
 
 /*** Teacher's note ***/
@@ -12,35 +15,41 @@ public class FlamingNinjaStar {
 		int flameSize = 200;		//the length of the flaming arms
 
 		// *14. Use the methods setX and setY to move the ninja star into the center of the screen
-		
+Tortoise.setX(1000);
+Tortoise.setY(350);
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
 		// 1. Show the Tortoise
 Tortoise.show();
 		// 12. Set the tortoise speed to 10
-
+Tortoise.setSpeed(10);
 		// 13. Make all the code below repeat 25 times
-
+for (int i = 0; i < 24; i++) {
+	
 		// 2. Turn the tortoise 1/8 of a circle
 Tortoise.turn(360/8);
 		// 3. Move the tortoise 64 pixels
+Tortoise.setPenColor(Color.BLACK);
 Tortoise.move(64);
 		// 4. Turn the tortoise 40 degrees to the LEFT. (Negative numbers will turn the tortoise counter-clockwise.)
-
+Tortoise.turn(-40);
 		// 5. Move the tortoise the distance in the variable flameSize
-			
+Tortoise.setPenColor(Colors.Reds.FireBrick);
+Tortoise.move(flameSize);
 		// 6. Turn the tortoise 170 degrees
-			
+Tortoise.turn(170);
 		// 7. Move the tortoise the distance in the variable flameSize (again)
-			
+Tortoise.setPenColor(Colors.Reds.FireBrick);
+Tortoise.move(flameSize);
 		// 8. Turn the tortoise 64 degrees to the right
-			
+Tortoise.turn(92);
 		// 9. Move the tortoise the distance in the variable baseSize
-			
+Tortoise.setPenColor(Color.BLACK);
+Tortoise.move(baseSize);
 		// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
-		
-		// 11. Color your ninja star like Figure 2.
-		
-	}
 
+		// 11. Color your ninja star like Figure 2.
+Tortoise.setPenColor(Colors.Reds.Crimson);
+	}
+	}
 }
