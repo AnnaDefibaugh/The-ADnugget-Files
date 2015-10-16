38402788c64952b3af5f385dcb57d1a93cd133ed
ADnugget <officialadnugget@gmail.com>
2015-10-15 18:25:29 -0700

    .

---
+++ src/BieverFever.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+public class BieverFever {
+	public static void main(String[] args) {
+		int i;
+		for (int j = 0; j < 2; j++) {
+		
+		for (i = 0; i < 3; i++) {
+			System.out.println("Baby");
+			
+		}
+		if (j == 0) {
+				System.out.println("Oh!");
+			}
+         
+     }
+		System.out.println("No!");
+		
+		System.out.println("☹ I thought you'd always be mine. ☹");
+   }
+ }
+	
+
+
+/*
+ * Bieber Fever challenge: 1: Write a program that uses a for loop to print 
+ * baby
+ * baby 
+ * baby 
+ * oh 
+ * Code should only contain one “baby”, and one “oh”
+ * 
+ * 2: Every 2nd time the “oh” should be “no”
+ * 
+ * 3: Each chorus ends with “I thought you'd always be mine”.
+ */
---
+++ src/PotatoCounter.java
@@ -0,0 +1,16 @@
+public class PotatoCounter {
+public static void main(String[] args) {
+	
+	for (int i = 1; i < 8; i++) {	
+	if (i == 4) {
+		System.out.println("4");
+		
+		}
+	else {
+			System.out.println(i+ " Tateretot");
+		}
+	
+	  }
+	System.out.println("More!");
+	}
+  }
